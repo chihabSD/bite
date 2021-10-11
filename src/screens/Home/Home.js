5e0b0979636cf9e2d5d3cfc0258345431c451ead
chihabsd <adamchihab2012@hotmail.com>
2021-10-11 21:28:32 +0100

    add horizontal menus and render items types

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, FlatList } from "react-native";
-import { dummyData, SIZES } from "../../../constants";
+import { View, Text, FlatList, TouchableOpacity } from "react-native";
+import { COLORS, dummyData, FONTS, SIZES } from "../../../constants";
 import HorizontalFoodCard from "./HorizontalFoodCard";
 import SearchHeader from "./SearchHeader";
 
@@ -13,6 +13,45 @@ const Home = ({ menuListS }) => {
     let selectedMenu = dummyData.menu.find((a) => a.id === menuTypeId);
     setMenuList(
       selectedMenu?.list.filter((a) => a.categories.includes(categoryId))
+    );
+  };
+  const renderMenuTypes = () => {
+    return (
+      <FlatList
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        data={dummyData.menu}
+        contentContainerStyle={{
+          marginTop: 30,
+          marginBottom: 30,
+        }}
+        keyExtractor={(item) => `${item.id}`}
+        renderItem={({ item, index }) => {
+          return (
+            <TouchableOpacity
+              onPress={() => {
+                setSelectedMenuType(item.id);
+                handleChangeCategory(selectedCategoryId, item.id);
+              }}
+              style={{
+                marginLeft: SIZES.padding,
+                marginRight:
+                  index == dummyData.menu.length - 1 ? SIZES.padding : 0,
+              }}
+            >
+              <Text
+                style={{
+                  ...FONTS.h3,
+                  color:
+                    selectedMenuType == item.id ? COLORS.primary : COLORS.black,
+                }}
+              >
+                {item.name}
+              </Text>
+            </TouchableOpacity>
+          );
+        }}
+      />
     );
   };
   useEffect(() => {
@@ -28,6 +67,7 @@ const Home = ({ menuListS }) => {
 
       <FlatList
         //   ref={flatListRef}
+        ListHeaderComponent={renderMenuTypes}
         showsHorizontalScrollIndicator={false}
         data={menuList}
         keyExtractor={(item) => `${item.id}`}
